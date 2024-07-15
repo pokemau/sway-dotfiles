525621614ad3b15f1427eb076fecfbf5da248017
pokemau <rentillosa90@gmail.com>
2024-07-15 22:16:29 +0800

    remove unused

--- install.sh
+++ install.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
 # ENABLE FLATPAK
+sudo dnf install flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # APPS
-sudo dnf install -y ark zsh neovim tmux gh gcc gcc-c++ alacritty \
+sudo dnf install -y ark kitty zsh neovim tmux gh gcc gcc-c++ alacritty \
 cmake nodejs qbittorrent vlc flatseal lxappearance obs-studio qt5ct \
-steam fedora-workstation-repositories flatpak discord cargo
+steam fedora-workstation-repositories cargo
 
 # CHROME
 sudo dnf config-manager --set-enabled google-chrome
-sudo dnf install google-chrome-stable
+sudo dnf install google-chrome-stable -y
 
 # VSCODE
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update
-sudo dnf install code
+sudo dnf install code -y
 
-# FLATPAKS
-flatpak install flathub com.discordapp.Discord
+mkdir -p ~/.local/share/fonts
 
-
-
-mkdir .local/share/fonts
-
-cp -r .icons/ .themes/ .tmux.conf .zshrc ~/
+cp -r .icons/ .themes/ .tmux.conf ~/
 cp -r fonts/* ~/.local/share/fonts
 
 cp -r Wallpapers ~/Pictures
@@ -33,9 +29,6 @@ cp -r Wallpapers ~/Pictures
 cd .config
 cp -r alacritty dunst kitty nvim rofi sway swaylock waybar xfce4 ~/.config
 
-
-echo "LOG IN TO GITHUB"
-gh auth login
-
 echo "OH MY ZSH"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ../.zshrc ~
